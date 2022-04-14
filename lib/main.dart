d4b9e03d861440dc887080fe30484aacd90a5050
tryggos16 <tryggvi.bogason@gmail.com>
2022-04-14 15:22:55 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  return runApp( const
+  MaterialApp(
+    home: BallPage(),
+  ),
+  );
+}
+
+class BallPage extends StatelessWidget {
+  const BallPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: const Center(
+          child: Text('Ask me anything'),
+        ),
+      ),
+      body: const Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  const Ball({Key? key}) : super(key: key);
+
+  @override
+  State<Ball> createState() => _BallState();
+}
+
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget> [
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                print('i got clicked');
+                setState(() {
+                  ballNumber = Random().nextInt(5) + 1;
+                  print('image number $ballNumber was picked');
+                });
+              },
+              child: Image.asset('images/ball$ballNumber.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
